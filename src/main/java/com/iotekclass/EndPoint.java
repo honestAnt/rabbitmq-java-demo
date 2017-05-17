47e8be956f7065898ab2d931c7963e521e56438a
honestAnt <wangfengbaowfb@163.com>
2017-05-17 18:05:07 +0800

    first commit

---
+++ src/main/java/com/iotekclass/EndPoint.java
@@ -0,0 +1,48 @@
+package com.iotekclass;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Created by honestAnt on 2017/5/17.
+ */
+public class EndPoint {
+    protected Channel channel;
+    protected Connection connection;
+    protected String endPointName;
+
+    public EndPoint(String endpointName) throws IOException {
+        this.endPointName = endpointName;
+
+        //Create a connection factory
+        ConnectionFactory factory = new ConnectionFactory();
+
+        //hostname of your rabbitmq server
+        factory.setHost("192.168.8.223");
+        factory.setUsername("admin");
+        factory.setPassword("iotek123");
+
+        //getting a connection
+        connection = factory.newConnection();
+
+        //creating a channel
+        channel = connection.createChannel();
+
+        //declaring a queue for this channel. If queue does not exist,
+        //it will be created on the server.
+        channel.queueDeclare(endpointName, false, false, false, null);
+    }
+
+
+    /**
+     * 关闭channel和connection。并非必须，因为隐含是自动调用的。
+     * @throws IOException
+     */
+    public void close() throws IOException{
+        this.channel.close();
+        this.connection.close();
+    }
+}
---
+++ src/main/java/com/iotekclass/Main.java
@@ -0,0 +1,34 @@
+package com.iotekclass;
+
+import java.util.HashMap;
+
+/**
+ * 测试主类
+ * Created by honestAnt on 2017/5/17.
+ */
+public class Main {
+    public Main() throws Exception{
+
+        QueueConsumer consumer = new QueueConsumer("queue");
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
+
+        Producer producer = new Producer("queue");
+
+        for (int i = 0; i < 100000; i++) {
+            HashMap message = new HashMap();
+            message.put("message number", i);
+            producer.sendMessage(message);
+            System.out.println("Message Number "+ i +" sent.");
+        }
+    }
+
+    /**
+     * @param args
+     * @throws SQLException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws Exception{
+        new Main();
+    }
+}
